4f326965bb9bcdcaef523bf680ab4e0378076ce8
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-06-29 23:47:21 -0400

    Referencing a message from an incoming call record

--- test/incoming_call_record.js
+++ test/incoming_call_record.js
@@ -7,6 +7,32 @@ var IncomingCallRecord = require('../lib/models/incoming_call_record')
 var RemoteMessage = require('../lib/models/remote_message')
 
 
+describe("The remote message", function(){
+    before(function(){
+    }); 
+    after(function(){
+
+    });
+    beforeEach(function(){
+
+    });
+    afterEach(function(done){
+        RemoteMessage.remove(function(err){
+            should.not.exist(err)
+            done()
+        })
+    });
+    it("is created", function(done){
+        var remote_message = new RemoteMessage()
+        remote_message.remote_url = '/api/v1/message/2' //this is the first reference to writeit
+        remote_message.save(function(err, documento){
+            should.not.exist(err)
+            documento.remote_url.should.equal(remote_message.remote_url)
+            done()
+        })
+    })
+})
+
 describe("The IncomingCallRecord", function(){
     before(function(){
     }); 
@@ -36,31 +62,34 @@ describe("The IncomingCallRecord", function(){
             done()
         })
     })
+    it("can have a remote message", function(done){
+        var record = IncomingCallRecord()
+        record.from = '+56 9739123123'
+        var remote_message = new RemoteMessage()
+        remote_message.remote_url = '/api/v1/message/3'
+        remote_message.save(function(err){
+            record.remote_message = remote_message
+            record.save(function(err, documento){
+                should.not.exist(err)
+
+                IncomingCallRecord.findOne({_id:documento._id})
+                    .populate('remote_message')
+                    .exec(function(err, retrieved_record){
+                        should.not.exist(err)
+                        should.exist(retrieved_record.remote_message)
+                        remote_message._id.equals(retrieved_record.remote_message._id).should.be.ok
+                        retrieved_record.remote_message.remote_url.should.equal(remote_message.remote_url)
+                        done()
+                    })
+                
+
+                
+            })
+        })
+        
+        
+    })
 });
 
-describe("The remote message", function(){
-    before(function(){
-    }); 
-    after(function(){
 
-    });
-    beforeEach(function(){
-
-    });
-    afterEach(function(done){
-        RemoteMessage.remove(function(err){
-            should.not.exist(err)
-            done()
-        })
-    });
-    it("is created", function(done){
-        var remote_message = new RemoteMessage()
-        remote_message.remote_url = '/api/v1/message/2' //this is the first reference to writeit
-        remote_message.save(function(err, documento){
-            should.not.exist(err)
-            documento.remote_url.should.equal(remote_message.remote_url)
-            done()
-        })
-    })
-})
 
