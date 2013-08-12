0101ac433c9006000fc88d616d85253c89094a3b
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-08-12 00:30:04 -0400

    Getting the answers from writeit

--- answer_updater.js
+++ answer_updater.js
@@ -1,4 +1,16 @@
-var answer_updater = function(){
+var request = require('request')
+var config = require('./config');
+var answer_updater = function(callback){
+	
+	var get_url = config.writeit_url;
+	var options = {
+        "headers":{"authorization":"ApiKey "+config.writeit_username+":"+config.writeit_key}
+    }
+	request.get(get_url ,options, function(error, response, body){
 
+	})
+	if(callback!=undefined){
+		callback()
+	}
 }
 module.exports.update = answer_updater
--- app.js
+++ app.js
@@ -89,7 +89,6 @@ app.post('/', function(req, res){
     }
     var post_url = config.writeit_url + '/api/v1/message/'
     request.post(post_url ,options, function(error, response, body){
-      console.log(error)
       var record = IncomingCallRecord();
       record.from = req.body.session.from.name;
       var remote_message = new RemoteMessage()
